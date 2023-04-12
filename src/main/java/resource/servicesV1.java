f085b1b2840948897a789cab5fd544080337e22c
Tim Mirkes <tlmirkes@madisoncollege.edu>
2023-04-12 10:49:21 -0500

    Updated javax to jakarta reference in class imports.

--- src/main/java/resource/servicesV1.java
+++ src/main/java/resource/servicesV1.java
@@ -6,9 +6,9 @@ import persistence.*;
 
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
 
